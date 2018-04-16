f3aaa1af96aaf8637b3cd2b785e53a4280e473af
Jaegeuk Kim <jaegeuk@google.com>
2018-04-16 14:39:54 -0700

    scsi: ufs: fix LINERESET on hibern8 without power hole
    This patch fixes the previous workaround which consumes lots of power by
    giving EINVAL on ufshcd_suspend().
    
    Bug: 78058462
    Bug: 77551464
    Fixes: d42e73ebf49a ("scsi: ufs: fix LINERESET on hibern8")
    Change-Id: I5660e5f097fa31bb0914669bda28598a1de062c2
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    [jjpprrrr: we had this commit in 2c4e3b96. Reapplying it here to
    cover the additional code in erasesize_show() and health_attr_show()]
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -10003,7 +10003,11 @@ erasesize_show(struct device *dev,
 		/* Got a valid one, and should be same as others. */
 		break;
 	}
-	pm_runtime_put_sync(hba->dev);
+	pm_runtime_mark_last_busy(hba->dev);
+	pm_runtime_put_noidle(hba->dev);
+	if (ret)
+		return 0;
+
 	return snprintf(buf, PAGE_SIZE, "0x%x\n", ret ? 0 : erasesize);
 }
 
@@ -10105,7 +10109,8 @@ static ssize_t health_attr_show(struct device *dev,
 	pm_runtime_get_sync(hba->dev);
 	err = ufshcd_read_desc(hba, QUERY_DESC_IDN_HEALTH, 0,
 					desc_buf, buff_len);
-	pm_runtime_put_sync(hba->dev);
+	pm_runtime_mark_last_busy(hba->dev);
+	pm_runtime_put_noidle(hba->dev);
 	if (err || (attr->bytes + attr->len) > desc_buf[0])
 		return 0;
 
