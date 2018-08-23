66b1d88975a8facb8685607b2c1915e65cd641ce
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-08-24 08:20:39 +0900

    kbuild: rename LDFLAGS to KBUILD_LDFLAGS
    Commit a0f97e06a43c ("kbuild: enable 'make CFLAGS=...' to add
    additional options to CC") renamed CFLAGS to KBUILD_CFLAGS.
    
    Commit 222d394d30e7 ("kbuild: enable 'make AFLAGS=...' to add
    additional options to AS") renamed AFLAGS to KBUILD_AFLAGS.
    
    Commit 06c5040cdb13 ("kbuild: enable 'make CPPFLAGS=...' to add
    additional options to CPP") renamed CPPFLAGS to KBUILD_CPPFLAGS.
    
    For some reason, LDFLAGS was not renamed.
    
    Using a well-known variable like LDFLAGS may result in accidental
    override of the variable.
    
    Kbuild generally uses KBUILD_ prefixed variables for the internally
    appended options, so here is one more conversion to sanitize the
    naming convention.
    
    I did not touch Makefiles under tools/ since the tools build system
    is a different world.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Reviewed-by: Palmer Dabbelt <palmer@sifive.com>
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -110,7 +110,7 @@ modpost_link()
 		info LD vmlinux.o
 	fi
 
-	${LD} ${LDFLAGS} -r -o ${1} $(modversions) ${objects}
+	${LD} ${KBUILD_LDFLAGS} -r -o ${1} $(modversions) ${objects}
 }
 
 # If CONFIG_LTO_CLANG is selected, we postpone running recordmcount until
@@ -136,7 +136,7 @@ vmlinux_link()
 
 	if [ "${SRCARCH}" != "um" ]; then
 		local ld=${LD}
-		local ldflags="${LDFLAGS} ${LDFLAGS_vmlinux}"
+		local ldflags="${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux}"
 
 		if [ -n "${LDFINAL_vmlinux}" ]; then
 			ld=${LDFINAL_vmlinux}
