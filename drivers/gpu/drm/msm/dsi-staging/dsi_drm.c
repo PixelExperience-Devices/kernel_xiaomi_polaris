69214a770b49524f7b9a85a6958df45757f298e8
Chenyang Zhong <zhongcy95@gmail.com>
2020-06-17 04:06:52 -0400

    drm/msm/dsi-staging: reduce log spam of panel already on
    [...] dsi-drm:[dsi_bridge_pre_enable] dsi_bridge_pre_enable panel already on
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -227,7 +227,7 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 			drm_notifier_call_chain(DRM_EVENT_BLANK, &g_notify_data);
 			dev->fp_quickon = false;
 		}
-		pr_info("%s panel already on\n", __func__);
+		pr_debug("%s panel already on\n", __func__);
 		return;
 	}
 
