933c34469db4554f5161b88f3bc9425f5ad44069
Chenyang Zhong <zhongcy95@gmail.com>
2021-06-01 13:59:56 -0400

    init: do not report the status of init_on_alloc and init_on_free
    The 4.9 kernel does not have those security features backported.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- init/main.c
+++ init/main.c
@@ -476,11 +476,7 @@ static void __init report_meminit(void)
 	else
 		stack = "off";
 
-	pr_info("mem auto-init: stack:%s, heap alloc:%s, heap free:%s\n",
-		stack, want_init_on_alloc(GFP_KERNEL) ? "on" : "off",
-		want_init_on_free() ? "on" : "off");
-	if (want_init_on_free())
-		pr_info("mem auto-init: clearing system memory may take some time...\n");
+	pr_info("mem auto-init: stack:%s\n",stack);
 }
 
 /*
