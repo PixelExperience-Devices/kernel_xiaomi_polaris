f64ae034c8600292d9b9dc863f370f15a4c04cb3
ReallySnow <reallysnow233@gmail.com>
2022-07-28 03:33:03 +0800

    erofs: Adapt to the memory allocation usage of 4.9
    Change-Id: I7676bfec707c1383c2de480bc0231e14f98089ab

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -7,6 +7,7 @@
 #include "compress.h"
 #include <linux/overflow.h>
 #include <linux/prefetch.h>
+#include <linux/vmalloc.h>
 
 #include <trace/events/erofs.h>
 
@@ -857,13 +858,15 @@ static int z_erofs_decompress_pcluster(struct super_block *sb,
 		   mutex_trylock(&z_pagemap_global_lock)) {
 		pages = z_pagemap_global;
 	} else {
-		gfp_t gfp_flags = GFP_KERNEL;
+		pages = kmalloc_array(nr_pages, sizeof(struct page *),
+				       GFP_KERNEL);
 
-		if (nr_pages > Z_EROFS_VMAP_GLOBAL_PAGES)
-			gfp_flags |= __GFP_NOFAIL;
+		if (!pages) 
+			pages = vmalloc(nr_pages * sizeof(struct page *));
 
-		pages = kvmalloc_array(nr_pages, sizeof(struct page *),
-				       gfp_flags);
+		if (!pages && nr_pages > Z_EROFS_VMAP_GLOBAL_PAGES)
+			pages = kmalloc_array(nr_pages, sizeof(struct page *),
+				       GFP_KERNEL | __GFP_NOFAIL);
 
 		/* fallback to global pagemap for the lowmem scenario */
 		if (!pages) {
@@ -1183,7 +1186,7 @@ jobqueue_init(struct super_block *sb,
 	struct z_erofs_decompressqueue *q;
 
 	if (fg && !*fg) {
-		q = kvzalloc(sizeof(*q), GFP_KERNEL | __GFP_NOWARN);
+		q = kzalloc(sizeof(*q), GFP_KERNEL | __GFP_NOWARN);
 		if (!q) {
 			*fg = true;
 			goto fg_out;
